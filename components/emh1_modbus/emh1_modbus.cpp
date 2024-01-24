35ad6f6ef5dd1810d798cd79aebeb61c896a1536
Jaap Vermaas <jaap@tuxic.nl>
2024-01-24 08:26:22 +0100

    fix flow_control_pin

--- components/emh1_modbus/emh1_modbus.cpp
+++ components/emh1_modbus/emh1_modbus.cpp
@@ -14,8 +14,6 @@ void eMH1Modbus::setup() {
   if (this->flow_control_pin_ != nullptr) {
      this->flow_control_pin_->setup();
   }
-	// pinMode(5, OUTPUT);
-	// digitalWrite(5, LOW);
 }
 
 void eMH1Modbus::loop() {
@@ -261,12 +259,10 @@ void eMH1Modbus::send(eMH1MessageT *tx_message) {
 	buffer[size++] = 0x0D;
 	buffer[size++] = 0x0A;
   ESP_LOGW(TAG, "TX -> %s", buffer);
-	// digitalWrite(5, HIGH);
   if (this->flow_control_pin_ != nullptr)
     this->flow_control_pin_->digital_write(true);
   this->write_array((const uint8_t *)buffer, size);
   this->flush();
-	// digitalWrite(5, LOW);
   if (this->flow_control_pin_ != nullptr)
     this->flow_control_pin_->digital_write(false);
 }
